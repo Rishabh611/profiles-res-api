22102f8c2101f9579f6aa2fad45a0c391b71aba4
Rishabh611 <44869319+Rishabh611@users.noreply.github.com>
2023-03-13 22:49:32 +0530

    added search to user profile

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from profiles_api import serializers
 from rest_framework import viewsets
-from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+
+from profiles_api import models
+from profiles_api import serializers
 from profiles_api import permissions
 
 
@@ -89,3 +91,5 @@ class ProfileViewset(viewsets.ModelViewSet):
     queryset = models.UserProfiles.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [permissions.UpdateOwnProfile]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["name", "email"]
